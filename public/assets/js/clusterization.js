da3981345ab249d4fbffd3b384770b858965d352
Zhilg <denisyakovlev02@yandex.ru>
2025-12-19 13:05:22 +0300

    Color Palette change in clusterization

--- public/assets/js/clusterization.js
+++ public/assets/js/clusterization.js
@@ -492,11 +492,11 @@ function updateSimilarityChart(distribution) {
 
 function getColorByIndex(index) {
     const colors = [
-        '#3498db',
-        '#2980b9', 
-        '#f39c12',
+        '#c0392b',
         '#e74c3c', 
-        '#c0392b'
+        '#f39c12',
+        '#abf000', 
+        '#00cc00'
     ];
     return colors[index] || '#3498db';
 }
